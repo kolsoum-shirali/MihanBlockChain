16e6d702f8572b73d51accbc8f55dd72069f8028
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-27 00:32:23 +0330

    feat : create TopNavbar component

--- components/Layout/Header/TopNavbar.vue
+++ components/Layout/Header/TopNavbar.vue
@@ -1,10 +1,32 @@
 <template>
-    <div>
-        
+  <div class="bg-[#2c2e32] text-[#ccc]">
+    <div class="w-[80%] mx-auto flex items-center justify-between">
+      <div class="flex items-center w-[20%]">
+        <i class="icon-clock text-xs"></i>
+        <span class="text-xs mr-1">جمعه 6 بهمن 1402</span>
+      </div>
+      <div class="w-[35%]">
+        <ul class="flex">
+          <li
+            v-for="(item, index) in options"
+            :key="index"
+            class="text-sm hover:bg-[#17181b] hover:cursor-pointer hover:text-[#38bdf8] pt-2 pb-3 px-3 transition-colors"
+          >
+            {{ item.title }}
+          </li>
+        </ul>
+      </div>
     </div>
-  </template>
-  
-  <script setup lang="ts"></script>
-  
-  <style lang="scss" scoped></style>
-  
+  </div>
+</template>
+
+<script setup lang="ts">
+const options = [
+  { title: "درباره ما" },
+  { title: "ارتباط با ما" },
+  { title: "فرصت‌های شغلی" },
+  { title: "تعرفه تبلیغات" },
+];
+</script>
+
+<style lang="scss" scoped></style>
