5811b2248b2b0ca116aaf1639872f9d512978448
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-28 18:05:30 +0330

    feat : install MainChartSlider component

--- components/Pages/Chart/MainChartSlider.vue
+++ components/Pages/Chart/MainChartSlider.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="bg-white p-10">
+  <div class="bg-white p-10 flex justify-between rounded-lg">
     <div class="w-[30%]">
       <PagesChartCryptoBox />
     </div>
-    <div class="w-[65%]"></div>
+    <div class="w-[68%]">
+      <PagesChartSliderChart />
+    </div>
   </div>
 </template>
 
