3cd704aeec746dcabddd3cc6ea2611e4cd87338b
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-28 09:16:30 +0330

    feat : create MainChartSlider component

---
+++ components/Pages/Chart/MainChartSlider.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-white p-10">
+    <div class="w-[30%]">
+      <PagesChartCryptoBox />
+    </div>
+    <div class="w-[65%]"></div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
