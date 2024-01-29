e03f7fb458159736edcee1e3f8bd4049f87159de
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-29 23:22:18 +0330

    feat : create WrapAnalyze Component

--- components/Pages/Home/Analyze/WrapAnalyze.vue
+++ components/Pages/Home/Analyze/WrapAnalyze.vue
@@ -1,7 +1,25 @@
 <template>
-  <div></div>
+  <div class="w-[80%] mx-auto py-10">
+    <div class="bg-black flex">
+      <div
+        v-for="(item, index) in tabs"
+        :key="index"
+        class="p-5 text-white hover:cursor-pointer transition-colors hover:bg-[#2c2e32] hover:text-[#38bdf8]"
+      >
+        <span>{{ item.title }}</span>
+      </div>
+    </div>
+    <PagesHomeAnalyzeSingleTabContent />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const tabs = [
+  { title: "تحلیل تکنیکال", number: 1 },
+  { title: "تحلیل آنچین", number: 2 },
+  { title: "تحلیل اقتصادی", number: 3 },
+  { title: "تحلیل فاندامنتال", number: 4 },
+];
+</script>
 
 <style lang="scss" scoped></style>
