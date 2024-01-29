c6f8494d541b52e0ac1136ca621eb51b940d1e16
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-29 21:56:29 +0330

    fix : WrapArticles component

--- components/Pages/Home/Content/WrapArticles.vue
+++ components/Pages/Home/Content/WrapArticles.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="flex justify-between mt-5">
-    <div class="w-[65%] bg-white rounded p-10">
-      <span class="caption text-xl font-bold pl-10 pr-3 pt-2 pb-3"
-        >آخرین مطالب</span
-      >
-      <PagesHomeContentMainLastArticles />
+    <div class="w-[65%]">
+      <div class="bg-white rounded p-10 border-2 border-[#ebe4e4]">
+        <PagesHomeContentMainLastArticles />
+      </div>
     </div>
-    <div class="w-[33%] bg-white rounded p-10">
-      <PagesHomeContentMostViewedArticles />
+    <div class="w-[33%]">
+      <div class="bg-white rounded p-10 border-2 border-[#ebe4e4]">
+        <PagesHomeContentMostViewedArticles />
+      </div>
+      <div class="bg-white rounded p-10 mt-10 border-2 border-[#ebe4e4]">
+        <PagesHomeContentCrypto />
+      </div>
     </div>
   </div>
 </template>
@@ -15,9 +19,4 @@
 <script setup></script>
 
 <style lang="scss" scoped>
-.caption {
-  background-color: #38bdf8;
-  color: white;
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 29% 100%);
-}
 </style>
