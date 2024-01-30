eba788640734c1a578d7bbd8acf435b92bd1113a
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-30 22:26:40 +0330

    feat : create BottomFooter component

---
+++ components/Layout/Footer/BottomFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-black py-5">
+  <div class="w-[80%] mx-auto text-white text-sm flex justify-between">
+    <p>
+      © 2024 - تمامی حقوق مادی و معنوی این وبسایت نزد میهن بلاکچین محفوظ است
+    </p>
+    <ul class="flex items-center">
+      <li v-for="(item, index) in items" :key="index" class="mx-2 hover:cursor-pointer hover:text-[#38bdf8] transition-colors">
+        {{ item.title }}
+      </li>
+    </ul>
+  </div>
+  </div>
+</template>
+
+<script setup>
+const items = [
+  { title: "درباره ما" },
+  { title: "ارتباط با ما" },
+  { title: "فرصت‌های شغلی " },
+  { title: "تعرفه تبلیغات" },
+];
+</script>
+
+<style lang="scss" scoped></style>
