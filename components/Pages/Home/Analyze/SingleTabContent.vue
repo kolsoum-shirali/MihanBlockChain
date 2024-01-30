b735e852f46839cdf139cd8ada9d8faa17f27758
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-30 13:17:03 +0330

    fix : change SingleTabContent component

--- components/Pages/Home/Analyze/SingleTabContent.vue
+++ components/Pages/Home/Analyze/SingleTabContent.vue
@@ -1,31 +1,38 @@
 <template>
-  <div class="bg-[#2c2e32] rounded text-white flex justify-between p-10 text-justify">
+  <div
+    class="bg-[#2c2e32] rounded text-white flex justify-between p-10 text-justify"
+  >
     <div class="w-[30%]">
       <div class="relative w-full h-[230px] rounded">
         <img
-          :src="`/_nuxt/assets/images/home/analyze/technical/${caption.img}`"
+          :src="`/_nuxt/assets/images/home/analyze/${singleTabContent.caption.address}/${singleTabContent.caption.img}`"
           class="w-full h-full rounded"
+          :key="1"
         />
         <nuxt-link
           to="/"
           class="article-type absolute z-10 bottom-1 left-1 text-white px-5 py-1 rounded-md transition-colors text-sm font-bold"
-          >{{ caption.type }}</nuxt-link
+          >{{ singleTabContent.caption.type }}</nuxt-link
         >
       </div>
-      <p class="text-lg font-bold mt-3">{{ caption.title }}</p>
+      <p class="text-lg font-bold mt-3 description">{{ singleTabContent.caption.title }}</p>
     </div>
     <div class="w-[66%] flex flex-wrap">
-      <div v-for="(item, index) in items" :key="index" class="w-[50%] flex">
+      <div
+        v-for="(item, index) in singleTabContent.items"
+        :key="index"
+        class="w-[50%] flex"
+      >
         <div class="w-[30%] h-[80px] rounded">
           <img
-            :src="`/_nuxt/assets/images/home/analyze/technical/${item.img}`"
+            :src="`/_nuxt/assets/images/home/analyze/${singleTabContent.caption.address}/${item.img}`"
             class="w-full h-full rounded"
           />
         </div>
         <div class="w-[70%]">
           <div class="py-2 px-4">
             <p
-              class="text-sm cursor-pointer hover:text-[#38bdf8] transition-colors"
+              class="description text-sm cursor-pointer hover:text-[#38bdf8] transition-colors"
             >
               {{ item.title }}
             </p>
@@ -37,40 +44,19 @@
 </template>
 
 <script setup>
-const caption = {
-  title:
-    "۲ روز تا جلسه فدرال رزرو برای تعیین نرخ بهره؛ ۵ نکته مهم که باید در هفته جاری مدنظر قرار داد",
-  img: "bitcoin-price-in-this-week.jpg",
-  type: "تحلیل تکنیکال",
-};
-const items = [
-  {
-    title:
-      "تحلیل تکنیکال قیمت بیت کوین، سولانا، آوالانچ، RNDR و سویی: ۹ بهمن ۱۴۰۲",
-    img: "1.jpg",
-  },
-  {
-    title: "تحلیل تکنیکال قیمت بیت کوین و ۹ آلت کوین منتخب بازار؛ ۷ بهمن ۱۴۰۲",
-    img: "2.jpg",
-  },
-  { title: "تحلیل آنچین و تکنیکال قیمت بیت کوین؛ ۱ بهمن ۱۴۰۲", img: "3.jpg" },
-  {
-    title:
-      "تحلیل تکنیکال قیمت بیت کوین و ۴ آلت کوین با پتانسیل رشد؛ ۲۵ دی ۱۴۰۲",
-    img: "4.jpg",
-  },
-  {
-    title: "تحلیل تکنیکال قیمت بیت کوین و ۹ آلت کوین منتخب بازار؛ ۲۳ دی ۱۴۰۲",
-    img: "5.jpg",
-  },
-  {
-    title: "آیا قیمت آربیتروم باز هم افزایش خواهد یافت؟ بررسی دلایل احتمالی",
-    img: "6.jpg",
-  },
-];
+const props = defineProps(["singleTabContent"]);
+const { singleTabContent } = props;
 </script>
 
 <style lang="scss" scoped>
+.description {
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+
 .article-type {
   background: linear-gradient(
     90deg,
