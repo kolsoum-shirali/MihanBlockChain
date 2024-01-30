7a120619123f38eeac112014fb95827d65754804
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-30 22:27:17 +0330

    feat : create MainFooterApp component

---
+++ components/Layout/Footer/MainFooterApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mt-10 bg-[#2c2e32]">
+    <div class="w-[80%] mx-auto flex flex-wrap justify-between py-14">
+      <div class="w-[70%]">
+        <LayoutFooterRightSectionFooter />
+      </div>
+      <div class="w-[25%]">
+        <LayoutFooterLeftSectionFooter />
+      </div>
+    </div>
+    <LayoutFooterBottomFooter />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
