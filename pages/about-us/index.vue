f88f7d0cb7614a5533784ab2742c62d5125c9559
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-31 20:13:44 +0330

    feat : create about-us page

---
+++ pages/about-us/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <LayoutHeaderMainHeaderApp />
+        <div class="bg-[#e9e9e9]">
+            <PagesHomeAboutUsMainAboutUs />
+        </div>
+        <LayoutFooterMainFooterApp />
+    </div>
+  </template>
+  <script></script>
+  
+  <style scoped></style>
+  
