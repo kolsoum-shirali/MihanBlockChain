3489a5ec34ad0a7af168523f9c752a523990c242
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-31 22:23:23 +0330

    feat : create default leyaout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div  class="bg-[#e9e9e9]">
+    <LayoutHeaderMainHeaderApp />
+    <div class="my-10">
+      <NuxtPage />
+    </div>
+    <LayoutFooterMainFooterApp />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
