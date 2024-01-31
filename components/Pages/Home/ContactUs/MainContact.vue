bbcba631ddf0333434566b29cfde9293f91bdf67
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-01-31 22:22:43 +0330

    feat : create MainContact component

---
+++ components/Pages/Home/ContactUs/MainContact.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-[80%] mx-auto bg-[#e9e9e9] flex justify-between">
+       <div class="w-[70%]">
+        <PagesHomeContactUsFormContactUs />
+       </div>
+       <div class="w-[28%]">
+        <PagesHomeContactUsSocialNetworks />
+       </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
