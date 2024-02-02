7c7edcf5c98e83d25cd1c6bde4bfdf21c3b76376
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-02-02 22:40:06 +0330

    feat : add servicesDrawer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,11 @@
 <template>
-  <div  class="bg-[#e9e9e9]">
+  <div  class="bg-[#e9e9e9] relative">
     <LayoutHeaderMainHeaderApp />
     <div class="my-10">
       <NuxtPage />
     </div>
     <LayoutFooterMainFooterApp />
+    <CommonServicesDrawer/>
   </div>
 </template>
 
