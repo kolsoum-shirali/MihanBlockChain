f799402d87f27177caf35e1d937b1e7a03d046c4
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-02-02 19:18:39 +0330

    feat : add path

--- components/Layout/Footer/BottomFooter.vue
+++ components/Layout/Footer/BottomFooter.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="bg-black py-5">
-  <div class="w-[85%] mx-auto text-white text-sm flex justify-between">
-    <p class="custom-font-12">
-      © 2024 - تمامی حقوق مادی و معنوی این وبسایت نزد میهن بلاکچین محفوظ است
-    </p>
-    <ul class="flex items-center">
-      <li v-for="(item, index) in items" :key="index" class="mx-2 hover:cursor-pointer hover:text-[#38bdf8] transition-colors custom-font-12">
+    <div class="w-[85%] mx-auto text-white text-sm flex justify-between">
+      <p class="custom-font-12">
+        © 2024 - تمامی حقوق مادی و معنوی این وبسایت نزد میهن بلاکچین محفوظ است
+      </p>
+      <ul class="flex items-center">
+        <nuxt-link
+          v-for="(item, index) in items"
+          :key="index"
+          :to="item.path"
+          class="single-item mx-2 hover:cursor-pointer hover:text-[#38bdf8] transition-colors custom-font-12"
+        >
+          {{ item.title }}
+        </nuxt-link>
+        <!-- <li v-for="(item, index) in items" :key="index" class="mx-2 hover:cursor-pointer hover:text-[#38bdf8] transition-colors custom-font-12">
         {{ item.title }}
-      </li>
-    </ul>
-  </div>
+      </li> -->
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
 const items = [
-  { title: "درباره ما" },
-  { title: "ارتباط با ما" },
-  { title: "فرصت‌های شغلی " },
-  { title: "تعرفه تبلیغات" },
+  { title: "درباره ما", path: "/about-us" },
+  { title: "ارتباط با ما", path: "/contact-us" },
+  { title: "فرصت‌های شغلی", path: "/jobs-opportunity" },
+  { title: "تعرفه تبلیغات", path: "/advertisement" },
 ];
 </script>
 
