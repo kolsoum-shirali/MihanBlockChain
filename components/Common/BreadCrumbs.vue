6b388bb70e4bcb1cc36ec63d343977cc28e14d5b
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-02-02 17:55:31 +0330

    feat : create BreadCrumbs component

---
+++ components/Common/BreadCrumbs.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-[#666] text-sm">
+    <span >میهن بلاکچین</span><span class="mx-2">/</span>
+    <span >اخبار</span><span class="mx-2">/</span>
+    <span class="font-bold text-[#38bdf8]">اخبار بیت کوین</span>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
