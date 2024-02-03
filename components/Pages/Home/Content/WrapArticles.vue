201f3f29754a743816fefa7a2b8af9250506a812
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-02-03 10:38:06 +0330

    feat : add sticky class

--- components/Pages/Home/Content/WrapArticles.vue
+++ components/Pages/Home/Content/WrapArticles.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="flex justify-between mt-5">
+  <div class="relative flex justify-between mt-5">
     <div class="w-[65%]">
       <div class="bg-white rounded p-10 border-2 border-[#ebe4e4]">
         <PagesHomeContentMainLastArticles />
       </div>
     </div>
     <div class="w-[33%]">
-      <div class="bg-white rounded p-10 border-2 border-[#ebe4e4]">
-        <PagesHomeContentMostViewedArticles />
+      <div class="sticky top-0 z-50">
+        <div class="bg-white rounded p-10 border-2 border-[#ebe4e4]">
+          <PagesHomeContentMostViewedArticles />
+        </div>
+        <CommonCryptoCade />
       </div>
-      <CommonCryptoCade />
-
     </div>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
