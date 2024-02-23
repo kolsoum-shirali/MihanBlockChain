c031c495230cb5517c4eb3744c5b4e2d66e2b64c
kolsoumshirali <kolsoumshirali2002@gmail.com>
2024-02-24 01:56:59 +0330

    feat : add title to breadCrumbs

--- components/Common/BreadCrumbs.vue
+++ components/Common/BreadCrumbs.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="text-[#666] text-sm">
+  <div class="text-[#666] text-sm leading-6">
     <span>میهن بلاکچین</span><span class="mx-2">/</span> <span>اخبار</span
     ><span class="mx-2">/</span>
-    <span class="font-bold text-[#38bdf8]">{{ breadCrumbs.category }}</span>
+    <span :class="{ 'text-[#38bdf8] font-bold': !breadCrumbs.title }">{{
+      breadCrumbs.category
+    }}</span>
+    <span class="mx-2" v-if="breadCrumbs.title">/</span>
+    <span class="font-bold text-[#38bdf8]">{{ breadCrumbs?.title }}</span>
   </div>
 </template>
 
