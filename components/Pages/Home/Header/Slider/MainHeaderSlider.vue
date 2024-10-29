d5b72890c464a9068e7bc2ab03cf1a58d7ab9db5
kolsoum <kolsoumshirali2002@gmail.com>
2024-10-29 21:33:48 +0330

    fix : imgs

--- components/Pages/Home/Header/Slider/MainHeaderSlider.vue
+++ components/Pages/Home/Header/Slider/MainHeaderSlider.vue
@@ -24,6 +24,11 @@
 
 <script setup>
 import oneImg from "@/assets/images/home/header/expire.jpg";
+import twoImg from "@/assets/images/home/header/fax.jpg";
+import threeImg from "@/assets/images/home/header/notCoin.jpg";
+import fourImg from "@/assets/images/home/header/artor.jpg";
+import fiveImg from "@/assets/images/home/header/etf.jpg";
+import sixImg from "@/assets/images/home/header/exchange.jpg";
 import "vue3-carousel/dist/carousel.css";
 import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
 const items = [
@@ -31,44 +36,38 @@ const items = [
     title: "نفت به بالاترین قیمت ۲ ماه اخیر رسید",
     description:
       "به دنبال رشد اقتصادی مثبت آمریکا و اخبار مربوط به بسته‌های محرک اقتصادی چین و همچنین بحران فعلی در منطقه",
-    img: "oneImg",
-    // img: "expire.jpg",
+    img: oneImg,
   },
   {
     title: "بررسی دلایل افزایش قیمت بیت کوین؛ همچنان احتمال ریزش وجود دارد",
     description:
       "بیت کوین بالاخره روز گذشته اندکی از فاز رریزشی خارج شد و پس از انقضای ۴.۵ میلیارد دلار از قراردادهای آپشن به بالای",
-    // img: "fax.jpg",
-    img: "oneImg",
+    img: twoImg,
   },
   {
     title: "تحلیل تکنیکال قیمت بیت کوین و ۹ آلت کوین منتخب بازار؛ ۷ بهمن ۱۴۰۲",
     description:
       "قیمت بیت کوین (BTC) در ساعات گذشته بیش از ۴٪ رشد کرده و برای لحظاتی از ۴۲٬۰۰۰ نیز دلار عبور کرده است. حال سوال",
-    // img: "notCoin.jpg",
-    img: "oneImg",
+    img: threeImg,
   },
   {
     title:
       "ایردراپ ۷۰۰ میلیون دلار ارز دیجیتال در روزهای آتی؛ فصل ایردراپ‌ها شروع شده است؟",
     description:
       "همزمان با صعودی شدن بازار ارزهای دیجیتال در ماه‌های گذشته، تعداد زیادی از پروژه‌ها از ایردراپ توکن خود بین کاربران",
-    // img: "artor.jpg",
-    img: "oneImg",
+    img: fourImg,
   },
   {
     title: "بلاکچین Xai چیست؟ بررسی شبکه گیمینگ زای و ارز دیجیتال XAI",
     description:
       "شبکه بازی زای (Xai) در ژوئن ۲۰۲۳ (خرداد) راه‌اندازی شد و در همین مدت کوتاه توانست به‌سرعت رشد کند و به یکی از",
-    // img: "etf.jpg",
-    img: "oneImg",
+    img: fiveImg,
   },
   {
     title: "گپ CME چیست؟ شکاف قیمت بیت کوین در بورس کالای شیکاگو",
     description:
       "احتمالا در تحلیل‌های بیت کوین، با مفهومی به نام گپ CME هم برخورد کرده‌اید. این گپ از تفاوت قیمت بسته شدن کندل",
-    // img: "exchange.jpg",
-    img: "oneImg",
+    img: sixImg,
   },
 ];
 const settings = {
