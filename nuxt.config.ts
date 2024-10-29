76844ffab5dfed34ef62bf7d989a7aa06ee15154
kolsoum <kolsoumshirali2002@gmail.com>
2024-10-29 20:47:02 +0330

    feat : config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/MihanBlockChain/',
+    buildAssetsDir: 'assets'
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css", "~/assets/scss/main.scss"],
   postcss: {
